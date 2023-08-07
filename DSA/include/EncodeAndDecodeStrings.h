955ad179500980dca345779bce1c34184a471647
hiltonUniverse <hackshilton@gmail.com>
2023-08-07 14:43:10 +0200

    Encode and decode strings

---
+++ DSA/include/EncodeAndDecodeStrings.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <QDebug>
+#include <iostream>
+#include <regex>
+
+class EncodeAndDecodeStrings
+{
+public:
+    /*
+     * Time complexity  : O(n)
+     * Space complixity : O(1)
+    */
+    std::string encode(std::vector<std::string> &strs)
+    {
+        std::string encoded_str;
+
+        for(const std::string& str : strs)
+        {
+            encoded_str += std::to_string(str.length()) + "#" + str;
+        }
+
+        return encoded_str;
+    }
+
+    /*
+     * Time complexity  : O(n)
+     * Space complixity : O(1)
+    */
+    std::vector<std::string> decode(std::string &str)
+    {
+        std::vector<std::string> decoded_strs;
+        for(size_t i = 0; i < str.length();)
+        {
+            const int hash = static_cast<int>(str.find("#", i));
+            const int length = stoi(str.substr(i, hash - i));
+            i = hash + length + 1;
+            decoded_strs.push_back(str.substr(hash + 1, length));
+        }
+
+        return decoded_strs;
+    }
+
+};
+
+//I KNOW NOT THE BEST WAY OF TESTING BUT CAN'T AFFORD TO SPEND TIME ON WRITING UNIT TEST ATM.
+namespace test
+{
+    //-----------------------------------
+    void encodeAndDecodeStringsTest()
+    {
+        //FIRST INPUT
+        std::vector<std::string> input{""};
+        EncodeAndDecodeStrings encoder_decoder;
+        std::string encoded_str = encoder_decoder.encode(input);
+        //must be true
+        qWarning() << (encoded_str == "0#");
+        qWarning() << (encoder_decoder.decode(encoded_str) == input);
+
+        //SECOND INPUT
+        std::vector<std::string> input_second{"i", "love", "lint", "code"};
+        std::string encoded_str_second = encoder_decoder.encode(input_second);
+        //must be true
+        qWarning() << (encoded_str_second == "1#i4#love4#lint4#code");
+        qWarning() << (encoder_decoder.decode(encoded_str_second) == input_second);
+
+    }
+}
