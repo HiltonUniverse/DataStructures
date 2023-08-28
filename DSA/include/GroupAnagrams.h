0c3dd583f65a6e96e124adc1eb572e63cafb3a56
hiltonUniverse <hackshilton@gmail.com>
2023-08-28 11:00:02 +0200

    Group Anagrams improved

--- DSA/include/GroupAnagrams.h
+++ DSA/include/GroupAnagrams.h
@@ -7,73 +7,80 @@
 class Solution
 {
 public:
-    /*
-    Overall time complexity: O(m*n)
-    O(m) * (O(n) + O(1)) + O(m) * O(1)
-    O(m*n) + O(m) + O(m)
-    removing least significat terms and keeping the most significant term : O(m*n)
 
-
-    Overall space complexity: O(m*n)
-
-    1. unordered_map<string, vector<string>> m:
-        The unordered_map m stores the key-value pairs for grouping the anagrams.
-        The space it occupies depends on the number of distinct keys and the number of anagrams associated with each key.
-        In the worst case, there can be 'n' distinct keys (where 'n' is the number of strings in strs),
-        and each key might have 'm' anagrams (the maximum number of anagrams). Therefore, the worst-case space complexity for m is O(n * m).
-
-    2.vector<vector<string>> result:
-        The result vector stores the grouped anagrams as the final output.
-        In the worst case, there can be 'n' groups (where 'n' is the number of strings in strs),
-        and each group might contain all the strings if all strings are anagrams of each other.
-        Therefore, the worst-case space complexity for result is O(n).
-
-    3. vector<int> count(26) and string key (inside the getKey function):
-        The count vector is used for counting character occurrences, and key is a string used as a hash key.
-        Both count and key have fixed sizes and do not depend on the input size. They take constant space.
-        The space complexity for count and key is O(1).
-
-    Summing up the space complexities of all the components:
-
-    O(n * m) (for unordered_map m) + O(n) (for vector<vector<string>> result) + O(1) (for vector<int> count(26) and string key) = O(n * m)
-
-    In summary, the space complexity of the groupAnagrams function is O(n * m), where 'n' is the number of strings in the input strs,
-    and 'm' is the maximum number of anagrams associated with a single key.
-    The space complexity is primarily determined by the unordered_map m, which stores the grouped anagrams.*/
-
-    std::vector<std::vector<QString>> groupAnagrams(const std::vector<QString>& strs)
+    //Time complexity : O(n * s log s)
+        //n = number of strings
+        //s = length of each string
+    std::vector<std::vector<QString>> groupAnagramsLogComplexity(std::vector<QString>& strs)
     {
-        std::unordered_map<QString, std::vector<QString>> comp;
-        for(size_t i = 0; i < strs.size(); ++i) //let size of str = m = O(m)
+        std::unordered_map<QString, std::vector<QString>> result;
+
+        //O(n * s log s)
+        for(auto str : strs) //O(n)
         {
-            QString key = getKey(strs[i]); // as deduced: O(n)
-            comp[key].push_back(strs[i]); // O(1)
+            QString before_sort = str;
+            std::sort(str.begin(), str.end()); //O(s log s) = overallsorting takes
+            result[str].push_back(before_sort);
         }
 
-        std::vector<std::vector<QString>> return_val;
-        for(auto& [_,val] : comp) //O(m)
+        std::vector<std::vector<QString>> final_result;
+        for(const auto &[key, value] : result)
         {
-            return_val.push_back(val); // O(1)
+            final_result.push_back(value);
         }
 
-        return return_val;
+        return final_result;
     }
 
-private:
-    QString getKey(const QString& str) // T = O(n) * O(1) + O(26) * O(1) = removing constants leaves us with complexity of: O(n)
+
+    //Time complexity: O(v*s) + O(v) = O(v*s)
+    //Space complexity: O(v * s) = worst case for each string we could have a vector
+    std::vector<std::vector<QString>> groupAnagrams(const std::vector<QString>& strs)
+    {
+        std::unordered_map<QString, std::vector<QString>> pairs;//O(s * v) = for each string we could have a vector
+        //v = vector length
+        //O(v) * O(s) = O(v * s)
+        for(auto val : strs) //O(v)
+        {
+            QString key = generateKey(val); //O(s)
+            pairs[key].push_back(val); //O(1)
+        }
+
+        std::vector<std::vector<QString>> result;
+        for(auto& [key, value] : pairs) //O(v) = worst case all strs are unique
+        {
+            result.push_back(value);
+        }
+
+        return result;
+    }
+
+    //-----------------------------------
+    // s = str length
+    //O(s) + O(26) = O(s)
+    QString generateKey(QString& str)
     {
         std::vector<int> count(26);
-        for(int i = 0; i < str.size(); ++i) //O(n)
+
+        for(char c : str.toStdString()) //O(s)
         {
-            count[str[i].unicode() - QString('a').unicode()->unicode()]++; //vector random access and value update = O(1)
+            count[c - 'a']++;
         }
 
         QString key;
-        for(const int& val : count) //O(26) = as we have 26 characters
+        for(int a : count)//O(26)
         {
-            key.append(QString::number(val)); //O(1) = adding elements to the end has constant time complexity unless reallocation is required then is O(n)
+            //This # addition is very important
+            //after we reach the count of 10 for a character, eg: b= 1, d= 10 and b=10, c = 1
+            //we will have key for b1 and d10: 01010
+            //and for b10 and c1             : 01010
+            //Now even thought they are not the same in count vector, they will be the same when convert to QString.
+            //so we add a # to make them look distinct.
+            // 0#1#0#10#
+            // 0#10#1#0#
+            key.append(QString::number(a) + "#");
         }
-
+        qWarning() << key;
         return key;
     }
 };
@@ -87,13 +94,21 @@ namespace test
         std::vector<QString> strs{"eat","tea","tan","ate","nat","bat","hilton","hiltno"};
         Solution solution;
 
-        auto result =solution.groupAnagrams(strs);
+        //auto result =solution.groupAnagrams(strs);
 
-        // expected to see:
+        /*// expected to see:
         //Anagrams :  std::vector("hilton", "hiltno")
         //Anagrams :  std::vector("bat")
         //Anagrams :  std::vector("tan", "nat");
         //Anagrams :  std::vector("eat", "tea", "ate")
+
+        for(const auto& res : result)
+        {
+            qWarning() << "Anagrams : " << res;
+        }*/
+
+        std::vector<QString> new_strs{"bdddddddddd","bbbbbbbbbbc"};
+        auto result = solution.groupAnagrams(new_strs);
 
         for(const auto& res : result)
         {
