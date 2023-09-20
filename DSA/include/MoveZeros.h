5e030864b1d95cb881da0d49a71af8915acf49b2
hiltonUniverse <hackshilton@gmail.com>
2023-09-20 13:45:27 +0200

    move zero update

--- DSA/include/MoveZeros.h
+++ DSA/include/MoveZeros.h
@@ -13,14 +13,14 @@ public:
         int right = 0;
         while(right < nums.size())
         {
-            if(nums[right] != 0)
+            if(nums[right] == 0)
             {
-                std::swap(nums[left], nums[right]);
-                ++left;
                 ++right;
             }
             else
             {
+                std::swap(nums[left], nums[right]);
+                ++left;
                 ++right;
             }
         }
